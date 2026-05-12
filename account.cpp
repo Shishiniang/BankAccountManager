801c5bb72324d02794c9939bc4f872252f114169
shishi23333 <shishi23333@gmail.com>
2026-05-13 04:02:15 +0800

    somehow broken

---
+++ account.cpp
@@ -0,0 +1,41 @@
+//Page 177, chapter 5.7
+#include "account.h"
+#include <cmath>
+#include <iostream>
+
+double SavingsAccount::total=0;
+SavingsAccount::SavingsAccount(int date,int id,double rate):id(id),balance(0),rate(rate),lastDate(date),accumulation(0){
+    std::cout<<date<<"\t#"<<id<<" is created"<<std::endl;
+}
+
+void SavingsAccount::record(int date, double amount){
+    accumulation=accumulate(date);
+    lastDate=date;
+    amount=floor(amount*100+0.5)/100;
+    balance+=amount;
+    std::cout<<date<<"\t#"<<id<<"\t"<<amount<<"\t"<<balance<<std::endl;
+}
+
+void SavingsAccount::deposit(int date,double amount){
+    record(date,amount);
+}
+
+void SavingsAccount::withdraw(int date,double amount){
+    if(amount>getBalance()){
+        std::cout<<"Error: not enough money"<<std::endl;
+    }else{
+        record(date,-amount);
+    }
+}
+
+void SavingsAccount::settle(int date){
+    double interest=accumulate(date)*rate/365;
+    if(interest!=0){
+        record(date,interest);
+    }
+    accumulation=0;
+}
+
+void SavingsAccount::show() const {
+    std::cout<<"#"<<id<<"\tBalance: "<<balance;
+}
---
+++ account.h
@@ -0,0 +1,31 @@
+//Page 177, chapter 5.7
+#ifndef __ACCOUNT_H__
+#define __ACCOUNT_H__
+
+class SavingsAccount{
+    private:
+        int id; //User ID
+        double balance; //User balance
+        double rate; //Year interest rate
+        int lastDate; //Last time balance changed date
+        double accumulation; //Sum of interest
+        static double total;
+
+        void record(int date,double amount);
+        double accumulate(int date) const{
+            return accumulation+balance*(date-lastDate);
+        }
+
+    public:
+        SavingsAccount(int date,int id,double rate);
+        int getId() const {return id;}
+        double getBalance() const {return balance;}
+        double getRate() const {return rate;}
+        static double getTotal(){return total;}
+        void deposit(int date, double amount);
+        void withdraw(int date,double amount);
+        void settle(int date);//call every Jan 1st
+        void show() const;
+};
+
+#endif //__ACCOUNT_H__
