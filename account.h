295db81647833cfae270b97af5ea6591330b241d
shishi23333 <shishi23333@gmail.com>
2026-05-15 05:26:24 +0800

    finish editing account.h

--- account.h
+++ account.h
@@ -1,4 +1,4 @@
-//Page 234, chapter 6.7
+//Page 290, chapter 7.7
 #ifndef __ACCOUNT_H__
 #define __ACCOUNT_H__
 #include "date.h"
@@ -13,6 +13,11 @@ class Account{
         Account(const Date &date,const std::string &id);
         void record(const Date &date,double amount,const std::string &desc);
         void error(const std::string &msg) const;
+    public:
+        const std::string &getId()const{return id;}
+        double getBalance()const{return balance;}
+        static double getTotal(){return total;}
+        void show()const;
 };
 class SavingsAccount : public Account{
     private:
@@ -20,14 +25,37 @@ class SavingsAccount : public Account{
         double rate;
     public:
         SavingsAccount(const Date &date,const std::string &id,double rate);
-        const std::string getId() const {return id;}
-        double getBalance() const {return balance;}
         double getRate() const {return rate;}
-        static double getTotal(){return total;}
         void deposit(const Date &date,double amount,const std::string &desc);
         void withdraw(const Date &date,double amount,const std::string &desc);
         void settle(const Date &date);//call every Jan 1st
-        void show() const;
+};
+class CreditAccount:public Account{
+    private:
+        Accumulator acc;
+        double credit;
+        double rate;
+        double fee;
+        double getDebt()const{
+            double balance=getBalance();
+            return (balance<0 ? balance:0);
+        }
+    public:
+        CreditAccount(const Date &date,const std::string &id,double credit,double rate,double fee);
+        double getCredit()const {return credit;}
+        double getRate()const{return rate;}
+        double getFee()const{return fee;}
+        double getAvailableCredit()const{
+            if(getBalance()<0){
+                return credit+getBalance();
+            }else{
+                return credit;
+            }
+        }
+        void deposit(const Date &date,double amount,const std::string &desc);
+        void withdraw(const Date &date,double amount,const std::string &desc);
+        void settle(const Date &date);
+        void show()const;
 };
 
 #endif //__ACCOUNT_H__
