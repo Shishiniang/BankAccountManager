8b195b6acba44d058ab6d66d7badd463df588da5
shishi23333 <shishi23333@gmail.com>
2026-05-15 06:34:10 +0800

    finish editing main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,47 @@
-//Page 234, chapter 6.7
-//5_11.cpp
+//Page 290, chapter 7.7
+//7_10.cpp
 #include "account.h"
 #include <iostream>
+#include <vector>
 
 int main(){
     Date date(2008,11,1);//begin date
-    //create a few accounts
-    SavingsAccount accounts[]={
-        SavingsAccount(date,"03755217",0.015),
-        SavingsAccount(date,"02342342",0.015)
-    };
-    const int n=sizeof(accounts)/sizeof(SavingsAccount);
+
+    //create some saving accounts
+    std::vector<SavingsAccount> SavingsAccounts;
+    SavingsAccounts.push_back(SavingsAccount(date,"03755217",0.015));
+    SavingsAccounts.push_back(SavingsAccount(date,"02342342",0.015));
+    int ns=SavingsAccounts.size();
+
+    //create some credit accounts
+    std::vector<CreditAccount> CreditAccounts;
+    CreditAccounts.push_back(CreditAccount(date,"C5392394",10000,0.0005,50));
+    int nc=SavingsAccounts.size();
 
     //some transactions in 11th month
-    accounts[0].deposit(Date(2008,11,5),5000,"salary");
-    accounts[1].deposit(Date(2008,11,25),10000,"sell stock 0323");
+    SavingsAccounts[0].deposit(Date(2008,11,5),5000,"salary");
+    CreditAccounts[0].withdraw(Date(2008,11,15),2000,"buy a cell");
+    SavingsAccounts[1].deposit(Date(2008,11,25),10000,"sell stock 0323");
+    //settle for credit cards
+    CreditAccounts[0].settle(Date(2008,12,1));
     //some transactions in 12th month
-    accounts[0].deposit(Date(2008,12,5),5500,"salary");
-    accounts[1].withdraw(Date(2008,12,20),4000,"buy a laptop");
+    CreditAccounts[0].deposit(Date(2008,12,1),2016,"repay the credit");
+    SavingsAccounts[1].deposit(Date(2008,12,5),5500,"salary");
+    //settle for all accounts
+    for(auto&& sa:SavingsAccounts){
+        sa.settle((Date(2009,1,1)));
+    }
+    for(auto&& ca:CreditAccounts){
+        ca.settle((Date(2009,1,1)));
+    }
     //print info of accounts
     std::cout<<std::endl;
-    for(int i=0;i<n;++i){
-        accounts[i].settle(Date(2009,1,1));
-        accounts[i].show();
+    for(auto&& sa:SavingsAccounts){
+        sa.show();
+        std::cout<<std::endl;
+    }
+    for(auto&& ca:CreditAccounts){
+        ca.show();
         std::cout<<std::endl;
     }
     std::cout<<"Total: "<<SavingsAccount::getTotal()<<std::endl;
