50c99a6c44068c9ed83127553da1a155249fb21b
shishi23333 <shishi23333@gmail.com>
2026-05-15 03:23:10 +0800

    before edit account.h

---
+++ accumulator.h
@@ -0,0 +1,29 @@
+//Chapter 7.7,page 290
+//accumulator.h
+#ifndef __ACCUMULATOR_H__
+#define __ACCUMULATOR_H__
+
+#include "date.h"
+class Accumulator{
+    private:
+        Date lastDate;
+        double value;
+        double sum;
+    public:
+        Accumulator(const Date &date,double value):lastDate(date),value(value),sum(0){}
+        double getSum(const Date &date)const{
+            return sum+value*date.distance(lastDate);
+        }
+        void change(const Date &date,double value){
+            sum=getSum(date);
+            lastDate=date;
+            this->value=value;
+        }
+        void reset(const Date &date,double value){
+            lastDate=date;
+            this->value=value;
+            sum=0;
+        }
+};
+
+#endif //__ACCUMULATOR_H__
