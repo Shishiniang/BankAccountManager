83ae2f187d7e0b7fb133b2678484b2c6b55a9d27
ssn <shishi23333@gmail.com>
2026-05-29 22:01:34 +0800

    9_4_fixed

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ragnar/BankAccountManager
+# Install script for directory: /home/ragnar/BankAccountManager/BankAccountManager
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ragnar/BankAccountManager/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/ragnar/BankAccountManager/BankAccountManager/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
