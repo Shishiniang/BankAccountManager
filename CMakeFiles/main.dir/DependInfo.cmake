23f5f61013730025975b71f5667aeaf2fda2a335
ssn <shishi23333@gmail.com>
2026-05-29 22:08:57 +0800

    f

---
+++ CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ragnar/BankAccountManager/BankAccountManager/src/account.cpp" "CMakeFiles/main.dir/src/account.cpp.o" "gcc" "CMakeFiles/main.dir/src/account.cpp.o.d"
+  "/home/ragnar/BankAccountManager/BankAccountManager/src/date.cpp" "CMakeFiles/main.dir/src/date.cpp.o" "gcc" "CMakeFiles/main.dir/src/date.cpp.o.d"
+  "/home/ragnar/BankAccountManager/BankAccountManager/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- include/Array.h
+++ include/Array.h
@@ -23,4 +23,93 @@ class Array{
 };
 
 
+//constructor
+template<class T>
+Array<T>::Array(int sz){
+    assert(sz>=0);
+    size=sz;
+    list=new T[size];
+}
+
+//deconstructor
+template<class T>
+Array<T>::~Array(){
+    delete []list;
+}
+
+//copy constructor, deep copy
+template<class T>
+Array<T>::Array(const Array<T>&a){
+    size=a.size;
+    list=new T[size];
+    for(int i=0;i<size;++i){
+        list[i]=a.list[i];
+    }
+}
+
+//operand overload "="
+template<class T>
+Array<T>& Array<T>::operator=(const Array<T>& rhs){
+    if(&rhs!=this){
+        return *this;
+    }
+    if(size!=rhs.size){
+        delete[] list;
+        size=rhs.size;
+        list=new T[size];
+    }
+    for(int i=0;i<size;++i){
+        list[i]=rhs.list[i];
+    }
+}
+
+//operand overload "[]"
+template<class T>
+T& Array<T>::operator[](int n){
+    assert(n>=0 && n<size);
+    return list[n];
+}
+
+template<class T>
+const T& Array<T>::operator[](int n)const{
+    assert(n>=0 && n<size);
+    return list[n];
+}
+
+//conversion to T* so it could function like C-style array
+//T* points to T* list in this class
+template<class T>
+Array<T>::operator T*(){
+    return list;
+}
+
+template<class T>
+Array<T>::operator const T*()const{
+    return list;
+}
+
+//return size of this array
+template<class T>
+int Array<T>::getSize() const{
+    return size;
+}
+
+//modify array size to sz
+template<class T>
+void Array<T>::resize(int sz){
+    assert(sz>=0);
+    if(sz==size){
+        return;
+    }
+    T* newList=new T[sz];
+    int n=(sz<size)?sz:size;
+    for(int i=0;i<n;++i){
+        newList[i]=list[i];
+    }
+    delete[] list;
+    list=newList;
+    size=sz;
+}
+
+
 #endif //Array.h
